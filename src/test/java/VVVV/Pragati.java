da77da204e2b3090ee53d8331461560301560458
Charvik <Charvik@LAPTOP-S0IS0QVR>
2022-05-04 10:34:24 +0530

    2222222222222222

---
+++ src/test/java/VVVV/Pragati.java
@@ -0,0 +1,19 @@
+package VVVV;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class Pragati {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
